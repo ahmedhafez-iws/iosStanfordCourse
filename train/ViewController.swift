6e4e1ef02462d72a3fe81e790e93d9ffa6a71097
ahmedhafez-iws <53041696+ahmedhafez-iws@users.noreply.github.com>
2020-03-27 23:32:32 +0200

    Initial Commit

---
+++ train/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  train
+//
+//  Created by IT Wolf MacBook 1 on 3/27/20.
+//  Copyright © 2020 IT Wolf MacBook 1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
